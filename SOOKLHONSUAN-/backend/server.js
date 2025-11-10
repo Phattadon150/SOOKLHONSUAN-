305a4b0a72770af5eafe7eae4251526d4d0927ec
Phattadon150 <phattadon_d@cmu.ac.th>
2025-11-10 21:20:04 +0700

    Initial commit: setup frontend & backend

---
+++ SOOKLHONSUAN-/backend/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "SookLhonSuan Backend Running 🌱" });
+});
+
+app.listen(4000, () => console.log("Server running on port 4000"));
