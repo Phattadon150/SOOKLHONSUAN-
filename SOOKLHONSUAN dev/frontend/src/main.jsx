0e6680ddc1d85f83b4ed123314db676bf58bfc8c
Phattadon150 <phattadon_d@cmu.ac.th>
2025-11-18 14:44:55 +0700

    change file name to evading error

---
+++ SOOKLHONSUAN dev/frontend/src/main.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
+import App from "./App.jsx";
+import "./index.css";
+
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+const GOOGLE_CLIENT_ID =
+  "176658208646-2gepfppci4p5pki77vagh78ltu76tn6q.apps.googleusercontent.com";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </GoogleOAuthProvider>
+  </React.StrictMode>
+);
